4795bf3ea5d948f8320c7813ae12ca2d7b00f469
Isaac Flath <Isaac.flath@gmail.com>
2021-10-01 22:41:23 -0400

    Update kaggledataset.py

--- kaggledataset.py
+++ kaggledataset.py
@@ -12,19 +12,26 @@ def run_bash(bashCommand,return=True):
 
 def update_datset(dataset_path,update_message):
     if os.path.exists(dataset_path/'.ipynb_checkpoints'): shutil.rmtree(dataset_path/'.ipynb_checkpoints')
-    os.system(f'''kaggle datasets version -p {dataset_path} -m "{update_message}" --dir-mode zip''')
+    bashCommand = f'''kaggle datasets version -p {dataset_path} -m "{update_message}" --dir-mode zip'''
+    process, output, error = run_bash(bashCommand)
+    return process, output, error
     
     
 def create_dataset(dataset_path,dataset_name):
     if not os.path.exists(dataset_path): os.makedirs(dataset_path)
-    os.system(f"kaggle datasets init -p {dataset_path}")
+    
+    bashCommand = f"kaggle datasets init -p {dataset_path}"
+    process, output, error = run_bash(bashCommand)
+        
     with open(dataset_path/'dataset-metadata.json','r') as f: txt = f.readlines()
     txt = '\n'.join(txt)
     txt = txt.replace("INSERT_TITLE_HERE",dataset_name)
     txt = txt.replace("INSERT_SLUG_HERE",dataset_name)
     with open(dataset_path/'dataset-metadata.json','w') as f: f.write(txt)  
-    os.system(f"touch {dataset_path/'test.txt'}")
-    os.system(f"kaggle datasets create -p {dataset_path}")
+
+    bashCommand = f"kaggle datasets create -p {dataset_path}"
+    process, output, error = run_bash(bashCommand)
+    return process, output, error             
 
 def download_dataset_metadata(dataset_path,dataset_id):
     '''example: kaggle datasets metadata -p /path/to/download zillow/zecon'''
@@ -46,22 +53,27 @@ def download_dataset(dataset_path,dataset_id,dataset_name,unzip=True):
     if str(output).find('404') == -1: 
         download_dataset_content(dataset_id)
         os.system(f"mv {dataset_name}.zip {dataset_path}")
-        if unzip: os.system(f"unzip {dataset_path/(dataset_name+'.zip')} -d {dataset_path}")
-    else: create_dataset(dataset_path,dataset_name)
+        if unzip: 
+                bashCommand = f"unzip {dataset_path/(dataset_name+'.zip')} -d {dataset_path}"
+                process, output, error = run_bash(bashCommand)
+    else: process, output, error = create_dataset(dataset_path,dataset_name)
+    return process, output, error
     
 def add_library_to_dataset(library,dataset_path,pip_cmd="pip3",):
     if not os.path.exists(dataset_path/library): os.makedirs(dataset_path/library)
-    print(f"{pip_cmd} download {library} -d {dataset_path/library}")
-    os.system(f"{pip_cmd} download {library} -d {dataset_path/library}")
+        
+    bashCommand = f"{pip_cmd} download {library} -d {dataset_path/library}")
+    process, output, error = run_bash(bashCommand)
+
     print(f"In kaggle kernal you will need to run special command to install from this")
     print(f"!pip install -Uqq {library} --no-index --find-links=file:///kaggle/input/your_dataset/")
-
+    return process, output, error
     
 if __name__ == "__main__":
     libraries = ['fastai','timm','torch','torchvision','huggingface']
     for library in libraries:
         download_dataset(Path(library),f'isaacflath/{library}',library,unzip=True)
-        add_library_to_dataset(library,Path(library),'pip')
+        add_library_to_dataset(library,Path(library))
         
         
     
